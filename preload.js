cc32648a49aea522111b1195a65bde19702c4cff
nperalta-tw <nperalta@telware.com>
2020-03-10 15:13:49 -0400

    testing v1

--- preload.js
+++ preload.js
@@ -1,6 +1,8 @@
+console.log("outside onload");
 window.onload = function () {
   const { ipcRenderer } = require('electron');
   const version = document.getElementById('version');
+  console.log("preload");
   ipcRenderer.send('app_version');
   ipcRenderer.on('app_version', (event, arg) => {
     ipcRenderer.removeAllListeners('app_version');
@@ -10,11 +12,13 @@ window.onload = function () {
   const message = document.getElementById('message');
   const restartButton = document.getElementById('restart-button');
   ipcRenderer.on('update_available', () => {
+    console.log("update-available");
     ipcRenderer.removeAllListeners('update_available');
     message.innerText = 'A new update is available. Downloading now...';
     notification.classList.remove('hidden');
   });
   ipcRenderer.on('update_downloaded', () => {
+    console.log("update-downloaded");
     ipcRenderer.removeAllListeners('update_downloaded');
     message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
     restartButton.classList.remove('hidden');
